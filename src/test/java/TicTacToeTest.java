c6ea6502c706f7224b4a35966d6e5790d82e535f
uncleanw <mr.antonchervak@gmail.com>
2020-03-05 18:28:11 +0300

    fix diagonal

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -51,4 +51,40 @@ class TicTacToeTest {
         assertEquals(4,fieldForTest.findMaxLines(Symbols.X));
         fieldForTest.clearAll(TicTacToe.size);
     }
+    @Test
+    void anotherFindMaxDiagonal(){
+            TicTacToe fieldForTest = new TicTacToe( 7);
+            fieldForTest.addSymbol(0,0, Symbols.X);
+            fieldForTest.addSymbol(1,1, Symbols.X);
+            fieldForTest.addSymbol(2,2, Symbols.X);
+            fieldForTest.addSymbol(3,3, Symbols.O);
+            fieldForTest.addSymbol(4,4, Symbols.O);
+            fieldForTest.addSymbol(5,5, Symbols.X);
+            fieldForTest.addSymbol(6,6, Symbols.X);
+            assertEquals(3,fieldForTest.findMaxLines(Symbols.X));
+            assertEquals(2,fieldForTest.findMaxLines(Symbols.O));
+    }
+    @Test
+    void symbolCheck(){
+        TicTacToe fieldForTest = new TicTacToe(3);
+        fieldForTest.addSymbol(1,1, Symbols.X);
+        assertThrows(IllegalArgumentException.class,()->fieldForTest.findMaxLines(Symbols.VOID));
+        assertThrows(IllegalArgumentException.class,()->fieldForTest.findMaxLines(Symbols.Error));
+    }
+    @Test
+    void anotherFindMaxLine(){
+        TicTacToe fieldForTest = new TicTacToe( 7);
+        fieldForTest.addSymbol(0,0, Symbols.X);
+        fieldForTest.addSymbol(0,1, Symbols.X);
+        fieldForTest.addSymbol(0,2, Symbols.X);
+        fieldForTest.addSymbol(0,3, Symbols.O);
+        fieldForTest.addSymbol(0,4, Symbols.O);
+        fieldForTest.addSymbol(0,5, Symbols.X);
+        fieldForTest.addSymbol(0,6, Symbols.X);
+        assertEquals(3,fieldForTest.findMaxLines(Symbols.X));
+        assertEquals(2,fieldForTest.findMaxLines(Symbols.O));
+        fieldForTest.addSymbol(1,4, Symbols.O);
+        fieldForTest.addSymbol(2,5, Symbols.O);
+        assertEquals(3,fieldForTest.findMaxLines(Symbols.O));
+    }
 }
